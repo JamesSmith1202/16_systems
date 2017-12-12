de66e3ddf64fce9492bee48834dd2064e0788a43
JamesSmith1202 <jsmith@stuy.edu>
2017-12-12 16:02:40 -0500

    swapped to use the right dingy ya feel me?

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,27 +10,27 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  int fd;//FIFO FD
+  int upstream;//FIFO FD
   long int * msg;//client fifo pipe
   if(mkfifo(PIPE_NAME, 0644)){//create the fifo
     printf("%s\n", strerror(errno));
   }
-  if((fd = open(PIPE_NAME, O_RDONLY)) == -1){//open it, blocking until client forms connection
+  if((upstream = open(PIPE_NAME, O_RDONLY)) == -1){//open it, blocking until client forms connection
     printf("%s\n", strerror(errno));
   }
-  if(read(fd, msg, sizeof(long int)) == -1){//read in the fifo name sent by client
+  if(read(upstream, msg, sizeof(long int)) == -1){//read in the fifo name sent by client
     printf("%s\n", strerror(errno));
   }
   if(!remove(PIPE_NAME)){//remove the fifo, making it an unnamed pipe
     printf("%s\n", strerror(errno));
   }
-  if((fd = open(msg, O_WRONLY)) == -1){//Connect to the client fifo
+  if((*to_client = open(msg, O_WRONLY)) == -1){//Connect to the client fifo
     printf("%s\n", strerror(errno));
   }
-  if((write(fd, msg, sizeof(long int))) == -1){//write the msg back to the pipe
+  if((write(upstream, msg, sizeof(long int))) == -1){//write the msg back to the pipe
     printf("%s\n", strerror(errno));
   }
-  return 0;//Server has done its side of the handshake
+  return fd;//Server has done its side of the handshake
 }
 
 
